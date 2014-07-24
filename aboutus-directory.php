8053a197e83d4f01e86feec54a7b1289a98f6133
dcubas16 <dcubas16@hotmail.com>
2014-07-24 17:06:19 -0500

    modified

--- aboutus-directory.php
+++ aboutus-directory.php
@@ -47,7 +47,7 @@ while ( $row = mysql_fetch_assoc ( $result ) ) {
 ================================================== -->
 <body class="body-style">
 	<div class="row background-image-style"
-		style="	background: url('resources/pages-styles/page-background.jpeg') no-repeat; background-size: cover;; margin-right: 0px; margin-left: 0px;">
+		style="background: url('resources/pages-styles/page-background.jpeg') no-repeat; background-size: cover;; margin-right: 0px; margin-left: 0px;">
 		<div class="col-md-2"></div>
 		<div class="col-md-8">
 			<?php include('webframes/header.php');?>
@@ -59,352 +59,360 @@ while ( $row = mysql_fetch_assoc ( $result ) ) {
 					<div class="">
 						<h1 id="mision-and-goals"
 							class="font-style-medium-title-dark page-header ">Directorio</h1>
-							
-						<p>Actualmente la Oficina General de Recursos Humanos, incluyendo sus Oficias dependientes, consta del directorio que se muestra a continuación:</p>	
-						<p>Nota: Para comunicarse con nosotros Usted debe llamar al 6197000, y seguido marcar el anexo de la oficina que corresponda</p>
-						<table class="table table-bordered" style="font-size: small;">
+
+						<p>Actualmente la Oficina General de Recursos Humanos, incluyendo
+							sus Oficias dependientes, consta del directorio que se muestra a
+							continuación:</p>
+						<p>Nota: Para comunicarse con nosotros Usted debe llamar al
+							6197000, y seguido marcar el anexo de la oficina que corresponda</p>
+						<table class="table table-bordered">
 							<thead>
-								<tr>
-									<th>Oficina</th>
-									<th>Servidores de la OGRRHH</th>
-									<th>Tipo de Servidor</th>
-									<th>Cargo</th>
-									<th>Anexo</th>
+								<tr style="background-color: rgb(112, 42, 42); color: white;">
+									<th
+										style="vertical-align: middle; text-align: center; font-weight: bold;">OFICINA</th>
+									<th
+										style="vertical-align: middle; text-align: center; font-weight: bold;">SERVIDORES
+										DE LA OGRRHH</th>
+									<th
+										style="vertical-align: middle; text-align: center; font-weight: bold;">TIPO
+										DE SERVIDOR</th>
+									<th
+										style="vertical-align: middle; text-align: center; font-weight: bold;">CARGO</th>
+									<th
+										style="vertical-align: middle; text-align: center; font-weight: bold;">ANEXO</th>
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td rowspan="11">JEFATURA DE LA OFICINA GENERAL DE RECURSOS HUMANOS</td>
-									<td>MARTINEZ FLORES, HÉCTOR ELVIS</td>
-									<td>DOCENTE PERM.</td>
+								<tr class="success text-align-center">
+									<td rowspan="10"
+										style="vertical-align: middle; text-align: center; font-weight: bold;">JEFATURA
+										DE LA OFICINA GENERAL DE RECURSOS HUMANOS</td>
+									<td>Martínez Flores, Héctor Elvis</td>
+									<td>Docente Permanente</td>
 									<td>Jefe de Oficina General</td>
 									<td>7605</td>
 								</tr>
-								<tr>
-									<td>PAGAN SOTO, MORAIMA EDITH</td>
-									<td>ADM. PERM</td>
-									<td>Jefe de la Unidad de Secretaría y Tramite Documentario</td>
+								<tr class="success">
+									<td>Pagan Soto, Moraima Edith</td>
+									<td>Administrativo Permanente</td>
+									<td>Jefe de la Unidad de Secretaría y Trámite Documentario</td>
 									<td>7601</td>
 								</tr>
-								<tr>
-									<td>ROQUE DELGADO, NELSON</td>
-									<td>ADM. PERM</td>
+								<tr class="success">
+									<td>Roque Delgado, Nelson</td>
+									<td>Docente Permanente</td>
 									<td>Apoyo Administrativo</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>PIÑA RONDON, GLADYS RAQUEL</td>
-									<td>ADM. PERM</td>
+								<tr class="success">
+									<td>Piña Rondon, Gladys Raquel</td>
+									<td>Docente Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>PRADO ORE, WILLIAM ERNESTO</td>
-									<td>ADM. PERM</td>
+								<tr class="success">
+									<td>Prado Ore, William Ernesto</td>
+									<td>Docente Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>NUÑEZ CUBAS, DIEGO ANTONIO</td>
-									<td>ADM. DES.</td>
+								<tr class="success">
+									<td>Núñez Cubas, Diego Antonio</td>
+									<td>Administrativo Designado</td>
 									<td>Jefe de la Oficina de Modernización e Informatización</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>PAREDES ALEGRE, MARIA FAUSTA</td>
+								<tr class="success">
+									<td>Paredes Alegre, Maria Fausta</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>FALERO MARTINEZ, YOLANDA NELIDA</td>
+								<tr class="success">
+									<td>Colcas Salas, Sheyla Irayda</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>COLCAS SALAS, SHEYLA IRAYDA</td>
-									<td>CAS</td>
-									<td></td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>CARPIO PONCE, GIOVANNA MARION</td>
+								<tr class="success">
+									<td>Carpio Ponce, Giovanna Marion</td>
 									<td>CAS</td>
 									<td>Recepción y Mesa de Partes</td>
 									<td>7600</td>
 								</tr>
-								<tr>
-									<td>RAMIREZ GARNIQUE, JORGE OSWALDO</td>
+								<tr class="success">
+									<td>Ramírez Garnique, Jorge Oswaldo</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td rowspan="10">OFICINA DE REMUNERACIONES Y OBLIGACIONES SOCIALES</td>
-									<td>REYES CAMPOS, BLADIMIR FLAVIO</td>
-									<td>CONTRATO PERSONAL</td>
+								<tr class="info">
+									<td rowspan="9"
+										style="vertical-align: middle; text-align: center; font-weight: bold;">OFICINA
+										DE REMUNERACIONES Y OBLIGACIONES SOCIALES</td>
+									<td>Reyes Campos, Bladimir Flavio</td>
+									<td>Contrato Personal</td>
 									<td>Jefe de la Oficina de Remuneraciones y Obligaciones
 										Sociales</td>
 									<td>7608</td>
 								</tr>
-								<tr>
-									<td>BELLIDO MOLINA, ENRIQUE EDMUNDO</td>
-									<td>ADM. DES.</td>
+								<tr class="info">
+									<td>Bellido Molina, Enrique Edmundo</td>
+									<td>Administrativo Designado</td>
 									<td>Jefe de la Unidad de Presupuesto Analítico</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>DEJO EGUSQUIZA JOHN CARLOS</td>
-									<td>ADM. PERM</td>
-									<td>Jefe de la Unidad de Análisis y Control Planillas</td>
+								<tr class="info">
+									<td>Dejo Egusquiza, John Carlos</td>
+									<td>Docente Permanente</td>
+									<td>Jefe de la Unidad de Análisis y Control de Planillas</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>MARTINEZ RAMOS JULIO CESAR</td>
-									<td>ADM. PERM</td>
-									<td></td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>FELIX SANCHEZ ARTURO WENDEL</td>
-									<td>ADM. PERM</td>
+								<tr class="info">
+									<td>Martínez Ramos, Julio Cesar</td>
+									<td>Docente Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>POVIS SORIANO, DORIS HILDA</td>
+								<tr class="info">
+									<td>Félix Sánchez, Arturo Wendel</td>
+									<td>Administrativo Permanente</td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr class="info">
+									<td>Ramírez Cueva, Mary Yolanda</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>RAMIREZ CUEVA, MARY YOLANDA</td>
+								<tr class="info">
+									<td>Azorsa Salazar, Luis Tulio</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>AZORSA SALAZAR, LUIS TULIO</td>
+								<tr class="info">
+									<td>Parra Ore, Cinddy Stefany</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>PARRA ORE, CINDDY STEFANY</td>
+								<tr class="info">
+									<td>Valdivieso Carreño, Christian Luis</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>VALDIVIESO CARREÑO, CHRISTIAN LUIS</td>
-									<td>CAS</td>
-									<td></td>
-									<td></td>
-								</tr>
-								<tr>
-									<td rowspan="15">OFICINA DE GESTIÓN Y ESCALAFÓN</td>
-									<td>SULCA BRANCACHO JUANA MERCEDES</td>
+								<tr class="warning">
+									<td rowspan="15"
+										style="vertical-align: middle; text-align: center; font-weight: bold;">OFICINA
+										DE GESTIÓN Y ESCALAFÓN</td>
+									<td>Sulca Brancacho, Juana Mercedes</td>
 									<td>CONTRATO PERSONAL</td>
 									<td>Jefe de la Oficina de Gestión y Escalafón</td>
 									<td>7607</td>
 								</tr>
-								<tr>
-									<td>RAMOS BULLON LUISA ISABEL</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Ramos Bullon Luisa Isabel</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ALIPAZAGA CAMACHO VIRGINIA YSIDORA</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Alipazaga Camacho Virginia Ysidora</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>CHAMORRO VALLADARES BETTY LUZ</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Chamorro Valladares Betty Luz</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>CURO LLANCE ELISA BEATRIZ</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Curo Llance Elisa Beatriz</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ANAMPA FLORES OSCAR ALBERTO</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Anampa Flores Óscar Alberto</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>CALDERON MALDONADO ARTURO ISAIAS</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Calderón Maldonado Arturo Isaías</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ALVARADO BORJA, ROXANA CECILIA</td>
+								<tr class="warning">
+									<td>Alvarado Borja, Roxana Cecilia</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>VILAXA LOAYZA, MIGUEL ANGEL</td>
+								<tr class="warning">
+									<td>Vilaxa Loayza, Miguel Ángel</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>JAQUE LLANOS LISBET TERESA</td>
+								<tr class="warning">
+									<td>Jaque Llanos Lisbet Teresa</td>
 									<td>ADM. DES.</td>
 									<td>Jefe de la Unidad de Control de Asistencia</td>
 									<td>7422</td>
 								</tr>
-								<tr>
-									<td>BENDEZU DELGADO DE LIMACO NORA ISABEL</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Bendezú Delgado de Limaco, Nora Isabel</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>RONDINEL SHUPINGAHUA, LILIANA</td>
+								<tr class="warning">
+									<td>Rondinel Shupingahua, Liliana</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ZAVALETA GONZALEZ JOSE LUIS</td>
-									<td>ADM. PERM</td>
+								<tr class="warning">
+									<td>Zavaleta González José Luis</td>
+									<td>Administrativo Permanente</td>
 									<td>Jefe de la Unidad de Escalafón</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>PARI CAPA, EDITH</td>
+								<tr class="warning">
+									<td>Pari Capa, Edith</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>BATALLA PAREDES, LIZANDRO GERARDO</td>
+								<tr class="warning">
+									<td>Batalla paredes, Lizandro Gerardo</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td rowspan="16">OFICINA DE PENSIONES Y BENEFICIOS SOCIALES</td>
-									<td>MONCADA ALFARO MARIA ESPERANZA</td>
+								<tr class="danger">
+									<td rowspan="16"
+										style="vertical-align: middle; text-align: center; font-weight: bold;">OFICINA
+										DE PENSIONES Y BENEFICIOS SOCIALES</td>
+									<td>Moncada Alfaro María Esperanza</td>
 									<td>CONTRATO PERSONAL</td>
 									<td>Jefe de la Oficina de Pensiones y Beneficios Sociales</td>
 									<td>7614</td>
 								</tr>
-								<tr>
-									<td>MIRANDA VALENCIA JOSE LUIS</td>
-									<td>ADM. PERM</td>
-									<td>Jefe€ de la Unidad de Beneficios Sociales</td>
+								<tr class="danger">
+									<td>Miranda Valencia José Luis</td>
+									<td>Administrativo Permanente</td>
+									<td>Jefe de la Unidad de Beneficios Sociales</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ALIPAZAGA CAMACHO TITO FORTUNATO</td>
-									<td>ADM. PERM</td>
-									<td></td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>MEJIA MUÑOZ GASTON SANTOS</td>
-									<td>ADM. PERM</td>
+								<tr class="danger">
+									<td>Alipázaga Camacho, Tito Fortunato</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ASCENCIO ALTAMIRANO VICTOR VICENTE</td>
-									<td>ADM. PERM</td>
+								<tr class="danger">
+									<td>Mejía Muñoz, Gastón Santos</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>CANSECO FRANCIA ANGEL GERARDO</td>
-									<td>ADM. PERM</td>
+								<tr class="danger">
+									<td>Ascencio Altamirano, Victor Vicente</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>GIRALDO RAMIREZ DE REYES ELSA NORMA</td>
-									<td>ADM. PERM</td>
+								<tr class="danger">
+									<td>Canseco Francia, Ángel Gerardo</td>
+									<td>Administrativo Permanente</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>NOMBERTO CHAVEZ, VICTOR MANUEL</td>
+								<tr class="danger">
+									<td>Giraldo Ramírez de Reyes, Elsa Norma</td>
+									<td>Administrativo Permanente</td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr class="danger">
+									<td>Nomberto Chávez, Victor Manuel</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>VIRTO JIMENEZ, EDGAR LUCIANO</td>
+								<tr class="danger">
+									<td>Virto Jiménez, Edgar Luciano</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>VALVERDE VALVERDE, EDITH</td>
+								<tr class="danger">
+									<td>Valverde Valverde, Edith</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>SILVA AZAÑEDO, DORIS ELIZABETH</td>
+								<tr class="danger">
+									<td>Silva Azañedo, Doris Elizabeth</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>SALAZAR HUILLCA, AMALIA ROCIO</td>
+								<tr class="danger">
+									<td>Salazar Huillca, Amalia Rocio</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>AQUINO GUILLEN, SANDY LUCIA</td>
+								<tr class="danger">
+									<td>Aquino Guillen, Sandy Lucia</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ESTEBAN CAMACUARI GEREMIAS FELIX</td>
-									<td>ADM. PERM</td>
+								<tr class="danger">
+									<td>Esteban Camacuari Geremias Félix</td>
+									<td>Administrativo Permanente</td>
 									<td>Jefe de la Unidad de Pensiones</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>ATALAYA SANCHEZ, NEFTALI VISTHER</td>
+								<tr class="danger">
+									<td>Atalaya Sánchez, Neftali Visther</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>VEGA MACHADO, MIGUEL ANGEL</td>
+								<tr class="danger">
+									<td>Vega Machado, Miguel Ángel</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
 								</tr>
-								<tr>
-									<td rowspan="3">OFICINA DE DESARROLLO Y BIENESTAR SOCIAL</td>
-									<td>HUIZA LUNA NORMA BALBINA</td>
-									<td>ADM. PERM</td>
-									<td>Jefe de la Oficina de Desarrollo y Bienestar Soc.</td>
+								<tr class="success">
+									<td rowspan="3"
+										style="vertical-align: middle; text-align: center; font-weight: bold;">OFICINA
+										DE DESARROLLO Y BIENESTAR SOCIAL</td>
+									<td>Huiza Luna Norma Balbina</td>
+									<td>Administrativo Permanente</td>
+									<td>Jefe de la Oficina de Desarrollo y Bienestar Social</td>
 									<td>7420</td>
 								</tr>
-								<tr>
-									<td>MARCHAN CRUZ SANTOS NELIDA</td>
-									<td>ADM. PERM</td>
+								<tr class="success">
+									<td>Marchan Cruz Santos Nélida</td>
+									<td>Administrativo Permanente</td>
 									<td>Jefe de la Unidad de Bienestar Social</td>
 									<td></td>
 								</tr>
-								<tr>
-									<td>LEON LUQUE, LUCERO MILAGROS</td>
+								<tr class="success">
+									<td>León Luque, Lucero Milagros</td>
 									<td>CAS</td>
 									<td></td>
 									<td></td>
--- aboutus.php
+++ aboutus.php
@@ -58,6 +58,82 @@ while ( $row = mysql_fetch_assoc ( $result ) ) {
 					<div class="">
 						<h1 id="mision-and-goals"
 							class="font-style-medium-title-dark page-header ">Nosotros</h1>
+
+						<p>La Oficina General de Recursos Humanos es el órgano de apoyo,
+							dependiente de la Dirección General de Administración, encargado
+							de planificar, normar y ejecutar los procesos técnicos en materia
+							de gestión del recurso humano de la Institución.</p>
+						<p>Las funciones que desarrolla en esta oficina son:</p>
+						<ul>
+							<li><p>Planificar, normar, organizar, conducir y controlar las
+									acciones del Sistema de Recursos Humanos de la Universidad
+									Nacional Mayor de San Marcos.</p></li>
+							<li><p>Proponer a la Alta Dirección las normas, planes, programas
+									y acciones para la gestión del Sistema de Recursos Humanos.</p></li>
+							<li><p>Aplicar las normas, instrumentos de gestión y procesos
+									técnicos en materia de desarrollo integral de los recursos
+									humanos, en concordancia con las necesidades institucionales y
+									de acuerdo a la normatividad vigente.</p></li>
+							<li><p>Fomentar una actitud proactiva en los servidores docentes,
+									administrativos y obreros y desarrollo del liderazgo de los
+									directivos de la Universidad a través de acciones sostenidas de
+									mejoramiento de su calidad personal y profesional.</p></li>
+							<li><p>Coordinar sus acciones con otras entidades públicas en
+									asuntos vinculados a sus funciones.</p></li>
+							<li><p>Formular el Presupuesto Analítico de Personal (PAP), en
+									coordinación con la Oficina General de Planificación.</p></li>
+							<li><p>Coordinar, asesorar y supervisar las acciones
+									descentralizadas del Sistema de Recursos Humanos con las
+									diversas dependencias de la Universidad.</p></li>
+							<li><p>Resolver, por delegación de Facultades, los asuntos de su
+									competencia.</p></li>
+							<li><p>Proponer a la Alta Dirección políticas y programas
+									específicos de planificación, organización, acceso, evaluación
+									de rendimiento, compensación y desarrollo de los recursos
+									humanos, acorde con los procesos establecidos por ley.</p></li>
+							<li><p>Promover el desarrollo personal, social y laboral así como
+									la capacitación técnica de los servidores, orientados al logro
+									de las metas y objetivos institucionales, en función de las
+									necesidades de la entidad y los resultados de la evaluación de
+									desempeño.</p></li>
+							<li><p>Proponer y coordinar la celebración de convenios
+									nacionales e internacionales orientados al mejoramiento de la
+									calidad del servicio y la capacitación y desarrollo de
+									competencias de los servidores.</p></li>
+							<li><p>Realizar investigaciones y desarrollar estrategias
+									prospectivas en materia de productividad, con el fin de hacer
+									mas eficiente la función pública en la Universidad.</p></li>
+							<li><p>Elaborar y proponer la normatividad para el otorgamiento
+									de reconocimientos, incentivos y estímulos al desempeño
+									desatacado de los servidores de la Universidad.</p></li>
+							<li><p>Mantener actualizado el archivo de Legajos de servidores y
+									ex servidores docentes, administrativos y obreros de la
+									Universidad.</p></li>
+							<li><p>Elaborar la Planilla Única de Remuneraciones y de
+									Pensiones, de acuerdo con los dispositivos legales vigentes,
+									vigilando registrar en ella únicamente a los servidores que
+									cumplan labores efectivas, así como consignar los conceptos que
+									legalmente correspondan abonarse.</p></li>
+							<li><p>Establecer estrategias para aprovechar los conocimientos y
+									experiencia de ex servidores docentes, administrativos y
+									obreros en beneficio del mejoramiento de la calidad del
+									servicio.</p></li>
+							<li><p>Llevar el registro de los cargos públicos que califican
+									como funcionario público y empleado de confianza e informar
+									periódicamente a las autoridades del Gobierno Central.</p></li>
+							<li><p>Asesorar a la Alta Dirección en los asuntos de su
+									competencia.</p></li>
+							<li><p>Otras que le asigne la Dirección General de
+									Administración.</p></li>
+						</ul>
+						<br>
+						<p>La Oficina General de Recursos Humanos tiene la estructura
+							orgánica siguiente :</p>
+						<li><p>Oficina de Gestión y Escalafón</p></li>
+						<li><p>Oficina de Remuneraciones y Obligaciones Sociales</p></li>
+						<li><p>Oficina de Pensiones y Beneficios sociales</p></li>
+						<li><p>Oficina de Desarrollo y Bienestar Social</p></li>
+						<li><p>Oficina de Modernización e Informatización</p></li>
 						<p>En esta sección Usted podra encontrar información referente a
 							la Oficina General de Recursos Humanos de la Universidad Nacional
 							Mayor de San Marcos; dentro de lo más relevante se tiene:</p>
